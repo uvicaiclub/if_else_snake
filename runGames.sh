3a33a92b23c8483b5776a67bc815d9f42b5c6600
Eric-Showers <ejs1357@hotmail.com>
2023-01-24 10:58:03 -0800

    Args for training length

--- runGames.sh
+++ runGames.sh
@@ -1,17 +1,19 @@
-# loop 100 times
-x=0
-while [ $x -le 5000 ]
+#!/bin/bash
+total_games=$1
+batch_size=$2
+
+
+for (( i = 0; i < $total_games; i = i + $batch_size ))
 do
-    for i in {1..500}
+    echo "Trained $i games"
+    for (( j = 0; j < $batch_size; j++ ))
     do
         # run the game, output to gamei.json
-        ./battlesnake  play -n sl_snake1 -u http://localhost:8002 -n sl_snake2 -u http://localhost:8001 >/dev/null 2>&1
+        ./battlesnake  play -n if_else -u http://localhost:8002 -n sl_snake -u http://localhost:8001 >/dev/null 2>&1
     done
     python3 parseGameData.py
     cd snakeSupervision
     python3 supervisor.py 2>/dev/null
     cd ..
     rm games/*
-    x=$(( $x + 500 ))
-    echo "Trained $x games"
 done
