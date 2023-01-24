927d52c2e6dcecb68d682c5e066faff46b41ae75
Eric-Showers <ejs1357@hotmail.com>
2023-01-23 17:01:29 -0800

    Run solo games concurrently

--- testSnakes.sh
+++ testSnakes.sh
@@ -1,10 +1,31 @@
+#!/bin/bash
+
+function solo_if_else() {
+    for i in {1..50}
+    do
+        ./battlesnake  play -g solo -n if_else -u http://localhost:8001
+    done
+}
+
+function solo_sl_basic() {
+    for i in {1..50}
+    do
+        ./battlesnake  play -g solo -n sl_basic -u http://localhost:8002
+    done
+}
+
+function solo_sl_conv() {
+    for i in {1..50}
+    do
+        ./battlesnake  play -g solo -n sl_conv -u http://localhost:8003
+    done
+}
+
 # Run solo games for each snake
-for i in {1..50}
-do
-    ./battlesnake  play -g solo -n if_else -u http://localhost:8001
-    ./battlesnake  play -g solo -n sl_basic -u http://localhost:8002
-    ./battlesnake  play -g solo -n sl_conv -u http://localhost:8003
-done
+solo_if_else &
+solo_sl_basic &
+solo_sl_conv &
+wait
 
 # Run each snake against each other snake
 for i in {1..50}
