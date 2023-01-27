c69c51ebdf118360b582e846258ee4a31a8e1232
Eric-Showers <ejs1357@hotmail.com>
2023-01-27 09:54:37 -0800

    New sample format

--- snakeSupervision/predictor.py
+++ snakeSupervision/predictor.py
@@ -12,6 +12,7 @@ class Predictor:
     def __init__(self, model_path: str):
         # Load model
         self.model = keras.models.load_model(f"snakeSupervision/{model_path}")
+        self.model_path = model_path
 
     def predict(self, game_state: typing.Dict, action: str, subject: str):
         board_width = game_state['board']['width']
@@ -36,28 +37,20 @@ class Predictor:
         for food in game_state['board']['food']:
             food_array[food['x']][food['y']] = 1
 
-        actions_array = np.zeros((4))
+        actions_array = np.zeros((board_width, board_height))
         if action == 'up':
-            actions_array[0] = 1
+            actions_array[0][0] = 1
         elif action == 'down':
-            actions_array[1] = 1
+            actions_array[0][1] = 1
         elif action == 'left':
-            actions_array[2] = 1
+            actions_array[0][2] = 1
         elif action == 'right':
-            actions_array[3] = 1
+            actions_array[0][3] = 1
+        nn_inputs = np.array(
+            [subject_head, subject_body, heads_array, bodies_array, food_array, actions_array]
+        )
+        nn_inputs = np.transpose(nn_inputs, (1, 2, 0)).reshape(1, 11, 11, 6)
 
-        # Flatten arrays into 1D arrays
-        subject_head = subject_head.flatten()
-        subject_body = subject_body.flatten()
-        heads_array = heads_array.flatten()
-        bodies_array = bodies_array.flatten()
-        food_array = food_array.flatten()
-        actions_array = actions_array.flatten()
-
-        # Combine arrays into one nn input array
-        nn_inputs = np.concatenate((
-            subject_head, subject_body, heads_array, bodies_array, food_array, actions_array
-        )).reshape(1, 609)
         # Predict probabilty of winning with this action
         prediction = (action, self.model(nn_inputs, training=False))
         return (prediction[0], float(prediction[1][0][0]))
